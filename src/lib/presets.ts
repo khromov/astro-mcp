74256db7f5d29169a0bf255093a7d83876cf2d6a
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-12-04 23:13:22 +0100

    merge

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -21,20 +21,22 @@ export type PresetConfig = {
 }
 
 const SVELTE_5_PROMPT =
-	'Always use Svelte 5 runes and Svelte 5 syntax. Runes do not need to be imported, they are globals. $state() runes are always declared using `let`, never with `const`. When passing a function to $derived, you must always use $derived.by().'
+	'Always use Svelte 5 runes and Svelte 5 syntax. Runes do not need to be imported, they are globals. $state() runes are always declared using `let`, never with `const`. When passing a function to $derived, you must always use $derived.by(() => ...).'
 
 export const presets: Record<string, PresetConfig> = {
-	'svelte-complete-new': {
+	'svelte-complete': {
 		title: '⭐️ NEW Svelte (Recommended preset)',
 		description: '',
 		owner: 'sveltejs',
 		repo: 'svelte.dev',
 		glob: [
+			// Svelte
 			'**/apps/svelte.dev/content/docs/svelte/**/*.md',
-			//'**/apps/svelte.dev/content/docs/kit/**/*.md'
+			// SvelteKit
+			'**/apps/svelte.dev/content/docs/kit/**/*.md'
 		],
 		ignore: [
-
+			// Svelte ignores
 			'**/apps/svelte.dev/content/docs/svelte/07-misc/04-custom-elements.md',
 			'**/apps/svelte.dev/content/docs/svelte/07-misc/06-v4-migration-guide.md',
 			'**/apps/svelte.dev/content/docs/svelte/07-misc/07-v5-migration-guide.md',
@@ -42,11 +44,18 @@ export const presets: Record<string, PresetConfig> = {
 			'**/apps/svelte.dev/content/docs/svelte/07-misc/xx-reactivity-indepth.md',
 			'**/apps/svelte.dev/content/docs/svelte/98-reference/21-svelte-legacy.md',
 			'**/apps/svelte.dev/content/docs/svelte/99-legacy/**/*.md',
-			//TODO
 			'**/apps/svelte.dev/content/docs/svelte/98-reference/30-runtime-errors.md',
 			'**/apps/svelte.dev/content/docs/svelte/98-reference/30-runtime-warnings.md',
 			'**/apps/svelte.dev/content/docs/svelte/98-reference/30-compiler-errors.md',
 			'**/apps/svelte.dev/content/docs/svelte/98-reference/30-compiler-warnings.md',
+			'**/xx-*.md',
+			// SvelteKit ignores
+			'**/apps/svelte.dev/content/docs/kit/25-build-and-deploy/*adapter-*.md',
+			'**/apps/svelte.dev/content/docs/kit/25-build-and-deploy/99-writing-adapters.md',
+			'**/apps/svelte.dev/content/docs/kit/30-advanced/70-packaging.md',
+			'**/apps/svelte.dev/content/docs/kit/40-best-practices/05-performance.md',
+			'**/apps/svelte.dev/content/docs/kit/40-best-practices/10-accessibility.md', // May the a11y gods have mercy on our souls
+			'**/apps/svelte.dev/content/docs/kit/60-appendix/**/*.md',
 			'**/xx-*.md'
 		],
 		prompt: SVELTE_5_PROMPT,
@@ -64,11 +73,14 @@ export const presets: Record<string, PresetConfig> = {
 		description: '',
 		owner: 'sveltejs',
 		repo: 'svelte.dev',
-		glob: [
-			'**/apps/svelte.dev/content/docs/kit/**/*.md',
-		],
+		glob: ['**/apps/svelte.dev/content/docs/kit/**/*.md'],
 		ignore: [
-			'**/apps/svelte.dev/content/docs/svelte/99-legacy/**/*.md',
+			'**/apps/svelte.dev/content/docs/kit/25-build-and-deploy/*adapter-*.md',
+			'**/apps/svelte.dev/content/docs/kit/25-build-and-deploy/99-writing-adapters.md',
+			'**/apps/svelte.dev/content/docs/kit/30-advanced/70-packaging.md',
+			'**/apps/svelte.dev/content/docs/kit/40-best-practices/05-performance.md',
+			'**/apps/svelte.dev/content/docs/kit/40-best-practices/10-accessibility.md', // May the a11y gods have mercy on our souls
+			'**/apps/svelte.dev/content/docs/kit/60-appendix/**/*.md',
 			'**/xx-*.md'
 		],
 		prompt: SVELTE_5_PROMPT,
@@ -80,5 +92,5 @@ export const presets: Record<string, PresetConfig> = {
 			removeHtmlComments: true,
 			normalizeWhitespace: true
 		}
-	},
+	}
 }
