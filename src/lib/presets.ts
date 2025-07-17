2dda49cf74893ab5da7dbecf65a32672d2f09993
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-07-17 02:25:09 +0200

    Update presets.ts

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -255,7 +255,7 @@ export function transformAndSortPresets(presetsObject: Record<string, PresetConf
 			key: key.toLowerCase(),
 			...value
 		}))
-		.sort((a, b) => a.key.localeCompare(b.key))
+		.sort();
 }
 
 /**
